95e6b637c8c4b5424c3f937092233c4bf53237b3
mouson <mouson@gmail.com>
2017-03-27 01:45:34 +0800

    實作跳出 exception

--- app/SampleFunction/ArrayUntil.php
+++ app/SampleFunction/ArrayUntil.php
@@ -4,5 +4,9 @@ function array_until($stopPoint, $arr)
 {
     $index = array_search($stopPoint, $arr);
 
+    if (false === $index) {
+        throw new InvalidArgumentException('Key does not exist in array');
+    }
+
     return array_slice($arr, 0, $index);
 }
